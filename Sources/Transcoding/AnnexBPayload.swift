0eab9fc611bf6428e7d1f9db6027962058ba67a1
0tak2 <0tak2.code@gmail.com>
2025-10-22 14:52:04 +0900

    fix: make AnnexBPayload.init public

--- Sources/Transcoding/AnnexBPayload.swift
+++ Sources/Transcoding/AnnexBPayload.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct AnnexBPayload: Sendable, Codable {
     public let annexBData: Data
     public let presentationTimestamp: TimeInterval
+
+    public init(annexBData: Data, presentationTimestamp: TimeInterval) {
+        self.annexBData = annexBData
+        self.presentationTimestamp = presentationTimestamp
+    }
 }
