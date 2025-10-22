05f3f098f29f11c048c4790fd4879ba93c59b463
0tak2 <0tak2.code@gmail.com>
2025-10-22 15:44:47 +0900

    feat: VideoDecoderAnnexBAdapter에서 Timestamp를 현재 디바이스 Host Time에 맞게 조정

--- Sources/Transcoding/VideoDecoderAnnexBAdaptor.swift
+++ Sources/Transcoding/VideoDecoderAnnexBAdaptor.swift
@@ -5,6 +5,12 @@ import OSLog
 // MARK: - VideoDecoderAnnexBAdaptor
 
 public final class VideoDecoderAnnexBAdaptor {
+    /// Sender의 첫 프레임 기준점
+    private var firstSenderHostTime: TimeInterval?
+    
+    /// Receiver가 첫 프레임을 받은 시점 (Receiver의 HostTime 기준)
+    private var firstReceiverHostTime: CMTime?
+
     // MARK: Lifecycle
 
     public init(
@@ -27,11 +33,13 @@ public final class VideoDecoderAnnexBAdaptor {
     }
 
     public func decode(_ payload: AnnexBPayload) {
+        let receiverHostTimePTS = self.convertPayloadToReceiverHostTime(payload)
+
         switch codec {
         case .h264:
-            decodeH264(payload.annexBData, pts: payload.presentationTimestamp)
+            decodeH264(payload.annexBData, pts: receiverHostTimePTS)
         case .hevc:
-            decodeHEVC(payload.annexBData, pts: payload.presentationTimestamp)
+            decodeHEVC(payload.annexBData, pts: receiverHostTimePTS)
         }
     }
 
@@ -47,7 +55,7 @@ public final class VideoDecoderAnnexBAdaptor {
     var sps: Data?
     var pps: Data?
 
-    func decodeH264(_ data: Data, pts: TimeInterval? = nil) {
+    func decodeH264(_ data: Data, pts: CMTime? = nil) {
         for nalu in data.split(separator: H264NALU.startCode).map({ H264NALU(data: Data($0)) }) {
             if nalu.isSPS {
                 sps = nalu.data
@@ -63,12 +71,12 @@ public final class VideoDecoderAnnexBAdaptor {
                         Self.logger.error("Failed to create format description with error: \(error, privacy: .public)")
                     }
                 }
-                decodeAVCCFrame(nalu.avcc, pts: pts?.cmTime)
+                decodeAVCCFrame(nalu.avcc, pts: pts)
             }
         }
     }
 
-    func decodeHEVC(_ data: Data, pts: TimeInterval? = nil) {
+    func decodeHEVC(_ data: Data, pts: CMTime? = nil) {
         for nalu in data.split(separator: HEVCNALU.startCode).map({ HEVCNALU(data: Data($0)) }) {
             if nalu.isVPS {
                 vps = nalu.data
@@ -86,7 +94,7 @@ public final class VideoDecoderAnnexBAdaptor {
                         Self.logger.error("Failed to create format description with error: \(error, privacy: .public)")
                     }
                 }
-                decodeAVCCFrame(nalu.avcc, pts: pts?.cmTime)
+                decodeAVCCFrame(nalu.avcc, pts: pts)
             }
         }
     }
@@ -119,6 +127,31 @@ public final class VideoDecoderAnnexBAdaptor {
                 Self.logger.error("Failed to create sample buffer with error: \(error, privacy: .public)")
             }
         }
+    }
+
+    /// Payload의 상대 시간을 Receiver의 Host Time으로 변환한다.
+    private func convertPayloadToReceiverHostTime(_ payload: AnnexBPayload) -> CMTime {
+        
+        // 1. 첫 프레임 수신 시, Receiver의 기준점을 설정
+        if firstSenderHostTime == nil || firstReceiverHostTime == nil {
+            firstSenderHostTime = payload.firstFrameTimestamp
+            firstReceiverHostTime = CMClockGetTime(CMClockGetHostTimeClock())
+            
+            Self.logger.debug("Decoder synchronized clocks. First Sender TS: \(payload.firstFrameTimestamp)")
+            // 첫 프레임은 계산된 HostTime (지금)을 반환
+            return firstReceiverHostTime!
+        }
+        
+        // 2. 두 번째 프레임부터는 오프셋 기준으로 계산
+        
+        // (미디어 시간 경과) = (현재 프레임 PTS) - (첫 프레임 PTS)
+        let mediaDurationSeconds = payload.presentationTimestamp - payload.firstFrameTimestamp
+        
+        // (Receiver의 HostTime 기준 PTS 계산)
+        // targetHostTime = (Receiver의 첫 시간) + (미디어 시간 경과)
+        let targetHostTime = CMTimeAdd(firstReceiverHostTime!, mediaDurationSeconds.cmTime)
+        
+        return targetHostTime
     }
 }
 
